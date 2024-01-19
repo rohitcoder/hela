d8a4919c5b724f3e257b45a5516c2501ad76d0f5
Rohit kumar <rohittourister@gmail.com>
2024-01-19 15:56:35 +0530

    Disabled CIRCLE,PARSEUR,URI detectors

--- src/scans/tools/secret_tool.rs
+++ src/scans/tools/secret_tool.rs
@@ -57,7 +57,7 @@ impl SecretTool {
         let remove_git_folder = format!("rm -rf {}/.git", _path);
         execute_command(&remove_git_folder, true).await;
 
-        let cmd = format!("trufflehog filesystem --no-update {} --json --exclude-detectors=FLOAT,SIGNABLE,YANDEX,OANDA", _path);
+        let cmd = format!("trufflehog filesystem --no-update {} --json --exclude-detectors=FLOAT,SIGNABLE,YANDEX,OANDA,CIRCLE,PARSEUR,URI", _path);
         let output_data = execute_command(&cmd, true).await;
         let mut results: Vec<Value> = Vec::new();
         for line in output_data.lines() {
